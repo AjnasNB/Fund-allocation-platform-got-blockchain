730b74eb2903be464cdb8110d7a0118a463131f5
AjnasNB <ajnasnb@gmail.com>
2024-04-15 13:50:08 +0530

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Sidebar from './components/Sidebar';
+import Allocation from './pages/Allocation';
+import Deallocation from './pages/Deallocation';
+import EditAllocation from './pages/EditAllocation';
+import Allocated from './pages/Allocated';
+import HomePage from './pages/Home';
+import SetGovernmentEmployeePage from './pages/SetGovernmentEmployeePage';
+import RemoveGovernmentOfficialPage from './pages/RemoveGovernmentOfficialPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="flex">
+        <Sidebar />
+        <Routes>
+        <Route path="/" element={<HomePage />} />
+          <Route path="/allocate" element={<Allocation />} />
+          <Route path="/deallocate" element={<Deallocation />} />
+          <Route path="/edit" element={<EditAllocation />} />
+          <Route path="/allocated" element={<Allocated />} />
+          <Route path="/set-government-employee" element={<SetGovernmentEmployeePage />} />
+          <Route path="/remove-government-official" element={<RemoveGovernmentOfficialPage />} />
+
+
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
